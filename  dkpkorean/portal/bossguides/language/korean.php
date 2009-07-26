1a79438b0711abd19c41327a6d8e2585512c1f1e
coolxee <coolxee@d71197f6-76af-11de-8480-e142f2bfdb3b>
2009-07-26 01:05:02 +0000

    eqdkpkorean/trunk/portal/bossguides/language/korean.php Bossgguides - 한글 로컬파일 업로드

---
+++  dkpkorean/portal/bossguides/language/korean.php
@@ -0,0 +1,130 @@
+<?php
+/*
+ * Project:     EQdkp-Plus
+ * License:     Creative Commons - Attribution-Noncommercial-Share Alike 3.0 Unported
+ * Link:		http://creativecommons.org/licenses/by-nc-sa/3.0/
+ * -----------------------------------------------------------------------
+ * Began:       2008
+ * Date:        $Date: 2009-03-18 02:09:04 +0100 (Mi, 18 Mrz 2009) $
+ * -----------------------------------------------------------------------
+ * @author      $Author: osr-corgan $
+ * @copyright   2006-2008 Corgan - Stefan Knaak | Wallenium & the EQdkp-Plus Developer Team
+ * @link        http://eqdkp-plus.com
+ * @package     eqdkp-plus
+ * @version     $Rev: 4285 $
+ * 
+ * $Id: english.php 4285 2009-03-18 01:09:04Z osr-corgan $
+ */
+
+if ( !defined('EQDKP_INC') ){
+    header('HTTP/1.0 404 Not Found');exit;
+}
+
+/******Naxxramas*****/
+$lang['naxxramas']['naxxramas'] 	= array('long' => '낙스라마스', 'short' => '낙스');
+$lang['naxxramas']['anubrekhan'] 	= array('long' => '아눕레칸', 'short' => '아눕레칸');
+$lang['naxxramas']['faerlina'] 		= array('long' => '귀부인 펠리나', 'short' => '펠리나');
+$lang['naxxramas']['maexxna'] 		= array('long' => '맥스나', 'short' => '맥스나');
+$lang['naxxramas']['noth'] 			= array('long' => '역병술사 노스', 'short' => '노스');
+$lang['naxxramas']['heigan'] 		= array('long' => '부정의 헤이건', 'short' => '헤이건');
+$lang['naxxramas']['loatheb'] 		= array('long' => '로데브', 'short' => '로데브');
+$lang['naxxramas']['patchwerk'] 	= array('long' => '패치워크' , 'short' => '패치워크');
+$lang['naxxramas']['grobbulus'] 	= array('long' => '그라불르스', 'short' => '그라불르스');
+$lang['naxxramas']['gluth'] 		= array('long' => '글루스', 'short' => '글루스');
+$lang['naxxramas']['thaddius'] 		= array('long' => '타디우스', 'short' => '타디우스');
+$lang['naxxramas']['razuvious'] 	= array('long' => '훈련교관 라주비어스', 'short' => '라주비어스');
+$lang['naxxramas']['gothik'] 		= array('long' => '영혼착취자 고딕', 'short' => '고딕');
+$lang['naxxramas']['horseman'] 		= array('long' => '4인의 기사단', 'short' => '기사단');
+$lang['naxxramas']['sapphiron'] 	= array('long' => '사피론', 'short' => '사피론');
+$lang['naxxramas']['kelthuzad'] 	= array('long' => '켈투자드', 'short' => '켈투자드');
+$lang['naxxramas']['arachnid'] 		= array('long' => '거미 지구', 'short' => '거미 지구');
+$lang['naxxramas']['plague'] 		= array('long' => '역병 지구', 'short' => '역병 지구');
+$lang['naxxramas']['construct'] 	= array('long' => '피조물 지구', 'short' => '피조물 지구');
+$lang['naxxramas']['military'] 		= array('long' => '군사 지구', 'short' => '군사 지구');
+
+/******malygos*****/
+$lang['malygos']['malygos'] = array('long' => '말리고스', 'short' => '말리고스');
+
+/******sartharion*****/
+$lang['sartharion']['sartharion'] = array('long' => '살타리온', 'short' => '살타리온');
+
+
+/******ulduar*****/
+$lang['ulduar']['ulduar'] 				= array('long' => '울두아르', 'short' => '울두아르');
+$lang['ulduar']['hodir'] 				= array('long' => '호디르', 'short' => '호디르');
+$lang['ulduar']['thorim'] 				= array('long' => '토림', 'short' => '토림');
+$lang['ulduar']['council'] 				= array('long' => '무쇠 평의회', 'short' => '무쇠 평의회');
+$lang['ulduar']['freya'] 				= array('long' => '프레이야', 'short' => '프레이야');
+$lang['ulduar']['ignis'] 				= array('long' => '용광로 군주 이그니스', 'short' => '이그니스');
+$lang['ulduar']['leviathan'] 			= array('long' => '거대 화염전차', 'short' => '거대 화염전차');
+$lang['ulduar']['vezax'] 				= array('long' => '장군 베작스', 'short' => '장군 베작스');
+$lang['ulduar']['razorscale'] 			= array('long' => '칼날비늘', 'short' => '칼날비늘');
+$lang['ulduar']['deconstructor'] 		= array('long' => 'XT 002 해체자', 'short' => '해체자');
+$lang['ulduar']['kologarn'] 			= array('long' => '콜로간', 'short' => '콜로간');
+$lang['ulduar']['auriaya'] 				= array('long' => '아우리아야', 'short' => '아우리아야');
+$lang['ulduar']['mimiron'] 				= array('long' => '미미론', 'short' => '미미론');
+$lang['ulduar']['saron'] 				= array('long' => '요그사론', 'short' => '요그사론');
+$lang['ulduar']['algalon'] 				= array('long' => '관찰자 알갈론', 'short' => '알갈론');
+
+$bosslinks['WoW']['naxxramas']=array(
+	'arachnid' 		=> 'http://www.wowwiki.com/Naxxramas#Arachnid_Quarter',
+	'anubrekhan' 	=> 'http://wow.inven.co.kr/dataninfo2/indun/detail.php?named=23',
+	'faerlina' 		=> 'http://wow.inven.co.kr/dataninfo2/indun/detail.php?named=24',
+	'maexxna' 		=> 'http://wow.inven.co.kr/dataninfo2/indun/detail.php?named=25',
+	'plague' 		=> 'http://www.wowwiki.com/Naxxramas#Plague_Quarter',
+	'noth' 			=> 'http://wow.inven.co.kr/dataninfo2/indun/detail.php?named=29',
+	'heigan' 		=> 'http://wow.inven.co.kr/dataninfo2/indun/detail.php?named=30',
+	'loatheb' 		=> 'http://wow.inven.co.kr/dataninfo2/indun/detail.php?named=31',
+	'construct' 	=> 'http://www.wowwiki.com/Naxxramas#Construct_Quarter',
+	'patchwerk' 	=> 'http://wow.inven.co.kr/dataninfo2/indun/detail.php?named=19',
+	'grobbulus' 	=> 'http://wow.inven.co.kr/dataninfo2/indun/detail.php?named=20',
+	'gluth' 		=> 'http://wow.inven.co.kr/dataninfo2/indun/detail.php?named=21',
+	'thaddius' 		=> 'http://wow.inven.co.kr/dataninfo2/indun/detail.php?named=22',
+	'military' 		=> 'http://www.wowwiki.com/Naxxramas#Military_Quarter',
+	'razuvious' 	=> 'http://wow.inven.co.kr/dataninfo2/indun/detail.php?named=26',
+	'gothik' 		=> 'http://wow.inven.co.kr/dataninfo2/indun/detail.php?named=27',	
+	'horseman' 		=> 'http://wow.inven.co.kr/dataninfo2/indun/detail.php?named=28',
+	'sapphiron' 	=> 'http://wow.inven.co.kr/dataninfo2/indun/detail.php?named=32',
+	'kelthuzad' 	=> 'http://wow.inven.co.kr/dataninfo2/indun/detail.php?named=33'
+);
+
+$bosslinks['WoW']['malygos']=array(
+	'malygos' 		=> 'http://wow.inven.co.kr/dataninfo2/indun/detail.php?named=35'
+);
+
+$bosslinks['WoW']['sartharion']=array(
+	'sartharion' 	=> 'http://wow.inven.co.kr/dataninfo2/indun/detail.php?named=34',	
+);
+
+
+$bosslinks['WoW']['ulduar']=array(
+	'ulduar' 					=> 'http://wow.inven.co.kr/dataninfo2/indun/detail.php?indun=10',	
+	'hodir' 					=> 'http://wow.inven.co.kr/dataninfo2/indun/detail.php?named=42',	
+	'thorim' 					=> 'http://wow.inven.co.kr/dataninfo2/indun/detail.php?named=40',	
+	'council' 					=> 'http://wow.inven.co.kr/dataninfo2/indun/detail.php?named=46',	
+	'freya' 					=> 'http://wow.inven.co.kr/dataninfo2/indun/detail.php?named=44',	
+	'ignis'	 					=> 'http://wow.inven.co.kr/dataninfo2/indun/detail.php?named=49',	
+	'leviathan' 				=> 'http://wow.inven.co.kr/dataninfo2/indun/detail.php?named=48',	
+	'vezax' 					=> 'http://wow.inven.co.kr/dataninfo2/indun/detail.php?named=59',	
+	'razorscale' 				=> 'http://wow.inven.co.kr/dataninfo2/indun/detail.php?named=51',	
+	'deconstructor' 			=> 'http://wow.inven.co.kr/dataninfo2/indun/detail.php?named=53',	
+	'kologarn' 					=> 'http://wow.inven.co.kr/dataninfo2/indun/detail.php?named=57',	
+	'auriaya' 					=> 'http://wow.inven.co.kr/dataninfo2/indun/detail.php?named=61',	
+	'mimiron' 					=> 'http://wow.inven.co.kr/dataninfo2/indun/detail.php?named=63',	
+	'saron' 					=> 'http://wow.inven.co.kr/dataninfo2/indun/detail.php?named=65',	
+	'algalon' 					=> 'http://wow.inven.co.kr/dataninfo2/indun/detail.php?named=55'
+);
+
+
+$plang = array_merge($plang, array(
+   'pk_bossguides_headtext'     => 'Bossguides',
+  'pk_hide_naxxramas'			=> 'hide Naxxramas',
+  'pk_hide_malygos'  			=> 'hide Malygos',
+  'pk_hide_sartharion'     		=> 'hide Sartharion',
+  'pk_hide_ulduar'     			=> 'hide Ulduar',   
+));
+
+$plang = array_merge($plang,$lang);
+$plang = array_merge($plang,$bosslinks);
+
+?>
